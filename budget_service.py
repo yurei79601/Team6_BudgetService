5f1ede32ae19816c4999d1346cb6bacb9683836f
rei.huang <rei.huang@linecorp.com>
2024-02-20 17:47:05 +0800

    add some test cases

--- budget_service.py
+++ budget_service.py
@@ -1,5 +1,6 @@
 from typing import List
 from dataclasses import dataclass
+from calendar import monthrange
 from datetime import datetime
 
 @dataclass
@@ -12,4 +13,27 @@ class BudgetService:
         pass
 
     def query(self, start, end):
-        return 0
+        if start > end:
+            return 0
+        total_budget = 0
+        monthly_budgets = self.get_all()
+        for budget in monthly_budgets:
+            budget_year = budget.year_month.year
+            budget_month = budget.year_month.month
+            days_in_month = monthrange(budget_year, budget_month)[1]
+
+            if start.year <= budget_year <= end.year and start.month <= budget_month <= end.month:
+                if budget_year == start.year and budget_month == start.month:
+                    if start.month == end.month and start.year == end.year:
+                        days_count = end.day - start.day + 1
+                    else:
+                        days_count = days_in_month - start.day + 1
+                    daily_budget = budget.amount / days_in_month
+                    total_budget += daily_budget * days_count
+                elif budget_year == end.year and budget_month == end.month:
+                    daily_budget = budget.amount / days_in_month
+                    total_budget += daily_budget * end.day
+                else:
+                    total_budget += budget.amount
+
+        return total_budget
--- test_budget_service.py
+++ test_budget_service.py
@@ -1,13 +1,60 @@
 from datetime import datetime
 import pytest
-from budget_service import BudgetService
+from budget_service import BudgetService, Budget
 
 
-def test_singel_date(mocker):
+def test_valid_date(mocker):
     budget_service = BudgetService()
+    mock_budgets = [Budget(year_month=datetime(2024, 2, 1), amount=310)]
     mocker.patch(
         "budget_service.BudgetService.get_all",
-        return_value=[{"year_month": datetime(2024, 1, 0), "amount": 31}],
+        return_value=mock_budgets,
+    )
+    actual_value = budget_service.query(datetime(2024, 2, 2), datetime(2024, 2, 1))
+    assert actual_value == 0
+
+
+def test_single_date(mocker):
+    budget_service = BudgetService()
+    mock_budgets = [Budget(year_month=datetime(2024, 1, 1), amount=31)]
+    mocker.patch(
+        "budget_service.BudgetService.get_all",
+        return_value=mock_budgets,
     )
     actual_value = budget_service.query(datetime(2024, 1, 1), datetime(2024, 1, 1))
     assert actual_value == 1
+
+
+def test_partial_month_apr(mocker):
+    budget_service = BudgetService()
+    mock_budgets = [Budget(year_month=datetime(2024, 4, 1), amount=300)]
+    mocker.patch(
+        "budget_service.BudgetService.get_all",
+        return_value=mock_budgets,
+    )
+    actual_value = budget_service.query(datetime(2024, 4, 1), datetime(2024, 4, 5))
+    assert actual_value == 50
+
+
+def test_partial_month_mar(mocker):
+    budget_service = BudgetService()
+    mock_budgets = [Budget(year_month=datetime(2024, 3, 1), amount=620)]
+    mocker.patch(
+        "budget_service.BudgetService.get_all",
+        return_value=mock_budgets,
+    )
+    actual_value = budget_service.query(datetime(2024, 3, 1), datetime(2024, 3, 5))
+    assert actual_value == 100
+
+def test_query_for_months(mocker):
+    budget_service = BudgetService()
+    mock_budgets = [
+        Budget(year_month=datetime(2024, 3, 1), amount=620),
+        Budget(year_month=datetime(2024, 4, 1), amount=300)
+    ]
+    mocker.patch(
+        "budget_service.BudgetService.get_all",
+        return_value=mock_budgets,
+    )
+    actual_value = budget_service.query(datetime(2024, 3, 30), datetime(2024, 4, 2))
+    assert actual_value == 60
